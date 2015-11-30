eceb657652cc1057868d18945e65e5bc1dd39441
ouzo <hiouzo@gmail.com>
2015-11-30 16:41:29 +0800

    Merge remote-tracking branch 'origin/master'

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,13 @@
-# coding:utf-8
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# -*- coding: utf-8 -*-
 import logging
 from django.shortcuts import render
 from django.conf import settings

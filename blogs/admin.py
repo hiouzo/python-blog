64d7eb73129d3156ce2b0bbc526429edd7a970d1
ouzo <hiouzo@gmail.com>
2015-11-30 19:54:14 +0800

    Merge remote-tracking branch 'origin/master'

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,10 +4,25 @@ from models import *
 
 # Register your models here.
 
+class ArticleAdmin(admin.ModelAdmin):
+
+    # #页面内容需要显示的参数参数.exclude是不显示的参数列举
+    # fields = ('title','desc','content','date_publish','user')
+    fieldsets = (
+        (None, {
+            'fields': ('title','desc','content','user')
+        }),
+        ('more', {
+            'classes': ('collapse',),
+            'fields': ('tag','category','is_recommend')
+        }),
+    )
+
+
 admin.site.register(User)
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(Links)
 admin.site.register(Ad)

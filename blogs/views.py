e41b205a22c624f2ffd9aa219e753866e5a6c3db
ouzo <hiouzo@gmail.com>
2016-03-16 18:35:29 +0800

    Merge remote-tracking branch 'origin/master'

--- blogs/views.py
+++ blogs/views.py
@@ -8,7 +8,8 @@ logger = logging.getLogger('blogs.views')
 
 def global_setting(request):
     return {'SITE_NAME': settings.SITE_NAME,
-            'SITE_BESC': settings.SITE_BESC,}
+            'SITE_BESC': settings.SITE_BESC,
+            'PRO_EMAIL': settings.PRO_EMAIL,}
 
 def index(request):
     #测试日志例子, 找不到sss.txt文件的时候 在log文件夹的error.log里会发现多一个错误
